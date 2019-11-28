fffe99b2f14c4f23b8c03525bc8b311ff0dd0af5
Marc <mrctrgrst@gmail.com>
2019-11-28 02:57:47 -0600

    Create and Edit. Needed fixed Edit

--- src/database.js
+++ src/database.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/diario-db-app', {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 })
 
 .then( db => console.log('Connetion successeful'))
--- src/routes/diario.js
+++ src/routes/diario.js
@@ -1,8 +1,46 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/notes', (req,res) =>{
+const Note = require('../models/Notes');
 
-    res.send('database diary');
+
+
+router.get('/notes/add', (req, res)=>{
+    res.render('notes/newnotes');
+});
+
+router.post('/notes/newnote', async(req, res)=>{
+    const {title, descripcion} =  req.body;
+    const errors = [];
+
+    if(!title){
+        errors.push({text: "Escriba un nombre"});
+    }
+    if (!descripcion){
+        errors.push({text: "Escriba una descripcion"});
+    }
+    if(errors.length > 0){
+        res.render('notes/newnotes',{
+            errors,
+            title,
+            descripcion
+        });
+    }
+    else{
+    const newNote = new Note({title,descripcion});
+       await newNote.save();
+       res.redirect('/notes'); 
+    }
+});
+
+router.get('/notes', async(req,res) =>{
+ const notes = await Note.find().sort({date:'desc'});
+ res.render('notes/allnotes', {notes});   
 })
+
+router.get('/notes/edit/:edit', async(req, res) =>{
+    const note = await Note.findById(req.params.id);
+    res.render('notes/editnotes', {note});
+});
+
 module.exports = router;
