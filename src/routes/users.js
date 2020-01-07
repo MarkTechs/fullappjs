5594c660ac64830b4748cbc8e1f2d8e09927d7b7
Marc <mrctrgrst@gmail.com>
2020-01-07 01:06:14 -0600

    login

--- src/routes/users.js
+++ src/routes/users.js
@@ -11,4 +11,30 @@ router.get('/users/signup', (req, res) => {
     res.render('users/signup');
 })
 
+router.post('/users/signup', (req, res)=>{
+   const { name, email, password, confirmpass} = req.body;
+   const errors = [];
+
+   if(name.length > 0){
+    errors.push({text: 'El nombre no puede estar vacio'});
+   }
+
+   if(email.length > 0){
+    errors.push({text: 'El email no puede estar vacio'});
+   }
+
+   if(password != confirmpass){
+       errors.push({text: 'Las contraseñas no son iguales'});
+   }
+   if(password.length < 6 ){
+       errors.push({text: 'La contraseña tiene menos de 6 caracteres'});
+   }
+   if(errors.length > 0){
+       res.render('users/signup', {errors,name, email, password, confirmpass});
+   }
+   else{
+       res.send('ok');
+   } 
+});
+
 module.exports = router;
